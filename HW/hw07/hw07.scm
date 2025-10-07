9638711d8160e47c5fa1d519662ee395bfc23df7
Louis Lu <yanlu8977@gmail.com>
2025-10-07 21:23:44 +0800

    finished hw07

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -1,15 +1,26 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+  (if (= exp 1)
+  base
+  (if (even? exp)
+  (pow (square base) (/ exp 2))
+  (* base (pow (square base) (/ (- exp 1) 2)))
+  )
+  )
+)
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
-        (* y y y))))
+      (let ((y x))
+        (repeatedly-cube (- n 1) (* y y y))
+      )
+  )
+)
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
