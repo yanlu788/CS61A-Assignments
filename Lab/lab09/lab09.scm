4472f365a95c25a15ece6f3d583dc073d85866cb
Louis Lu <yanlu8977@gmail.com>
2025-10-07 20:20:36 +0800

    finished lab09

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,23 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (cond 
+    ((< num1 num2) -1)
+    ((= num1 num2) 0)
+    ((> num1 num2) 1))
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (lambda (inc) (+ num inc))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x) (f (g x)))
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 
+  (if (= n 0)
+    (lambda (x) x)
+    (lambda (x) (f ((repeat f (- n 1)) x))))
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +29,9 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (define max (max a b))
+  (define min (min a b))
+  (if (zero? (modulo max min))
+  min
+  (gcd min (modulo max min))))
