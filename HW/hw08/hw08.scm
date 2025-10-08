62be56730d179d97caa94285982ab845de40b8ba
Louis Lu <yanlu8977@gmail.com>
2025-10-08 14:46:30 +0800

    finished hw08

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,47 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+    (if (null? s)
+        #t
+        (if (null? (cdr s))
+        #t
+        (if (> (car s) (car (cdr s)))
+        #f
+        (ascending? (cdr s))
+        )
+        )
+    )
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    (cond ((null? s) nil)
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else
+            (my-filter pred (cdr s))
+        )
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (cond 
+        ((null? lst1) (append lst2))
+        ((null? lst2) (append lst1))
+        (else
+            (append 
+                (cons (car lst1) nil)
+                (cons (car lst2) nil)
+                (interleave (cdr lst1) (cdr lst2))
+            )
+        )
+    )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) 
+    (if (null? s)
+        nil
+        (cons (car s)
+            (no-repeats 
+                (filter (lambda (x) (not (= x (car s))))
+                    (cdr s))
+            )
+        )
+    )
+)
